0808e1d30dc353cf4123ba945168812238694067
Sean Oliver <36770636+oliversno@users.noreply.github.com>
2018-08-14 17:54:58 -0400

    Expanded tests for all single functions
    Failing testSingleDiv

--- calc_tests.cpp
+++ calc_tests.cpp
@@ -18,3 +18,37 @@ void testSingleMult() {
   correct << "3*5\n";
   assert(os.str() == correct.str());
 }
+void testSingleDiv() {
+  Operator times{ type::mult, new Constant{ 3 }, new Constant{ 5 } }; // 3/5
+  ElementTree tree{ &times };
+  std::ostringstream os;
+  tree.parse(os);
+  std::ostringstream correct;
+  correct << "3/5\n";
+  assert(os.str() == correct.str());
+}
+void testSingleAdd() {
+  Operator times{ type::mult, new Constant{ 3 }, new Constant{ 5 } }; // 3+5
+  ElementTree tree{ &times };
+  std::ostringstream os;
+  tree.parse(os);
+  std::ostringstream correct;
+  correct << "3+5\n";
+  assert(os.str() == correct.str());
+}
+void testSingleSub() {
+  Operator times{ type::mult, new Constant{ 3 }, new Constant{ 5 } }; // 3-5
+  ElementTree tree{ &times };
+  std::ostringstream os;
+  tree.parse(os);
+  std::ostringstream correct;
+  correct << "3-5\n";
+  assert(os.str() == correct.str());
+}
+
+void runAllTests() {
+  testSingleMult();
+  testSingleDiv();
+  testSingleAdd();
+  testSingleSub();
+}
